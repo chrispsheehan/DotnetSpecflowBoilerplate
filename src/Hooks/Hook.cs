ba11d8ff8ac275b2284517085a26bdccfa3de78f
Chris Sheehan <chris_sheehan@hotmail.co.uk>
2021-12-22 14:11:53 +0000

    shell hooks added

--- src/Hooks/Hook.cs
+++ src/Hooks/Hook.cs
@@ -1,4 +1,5 @@
 using System;
+using BoDi;
 using TechTalk.SpecFlow;
 
 namespace boilerplate.Hooks
@@ -6,6 +7,36 @@ namespace boilerplate.Hooks
     [Binding]
     public class Hooks
     {
-        
+        [BeforeTestRun]
+        public static void BeforeTestRun()
+        {
+            Console.WriteLine("Starting test run");
+        }
+
+        [BeforeFeature]
+        public static void BeforeFeature(FeatureContext featureContext)
+        {
+            Console.WriteLine($"Starting Feature: {featureContext.FeatureInfo.Title}");
+        }
+
+        [BeforeScenario]
+        public void BeforeScenario(IObjectContainer objectContainer, ScenarioContext scenarioContext)
+        {
+            //objectContainer.RegisterInstanceAs(objectToRegister);
+
+            Console.WriteLine($"Starting Scenario: {scenarioContext.ScenarioInfo.Title}");            
+        }
+
+        [AfterScenario]
+        public void AfterScenario(ScenarioContext scenarioContext)
+        {
+            Console.WriteLine($"Finishing Scenario: {scenarioContext.ScenarioInfo.Title}");            
+        }
+
+        [AfterFeature]
+        public static void AfterFeature(FeatureContext featureContext)
+        {
+            Console.WriteLine($"Finishing Feature: {featureContext.FeatureInfo.Title}");            
+        }
     }
 }
